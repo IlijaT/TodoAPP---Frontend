64878fd7cbe4909cc664f35296b5558c6901e889
IlijaT <tatalovicilija@gmail.com>
2018-06-28 17:39:21 +0200

    register.vue file has created #ILTA-31

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
     <div class="container">
-      <h1>To DO Application</h1>
+      <h1>Login</h1>
       <div class="row">
         <div class="col-xs-12">
           <app-login></app-login>
         </div>
       </div>
-
     </div>
-    
+  <hr>
+    <div id="app">
+      <div class="container">
+        <h1>Register</h1>
+        <div class="row">
+          <div class="col-xs-12">
+            <app-register></app-register>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
   import Login from "./components/login.vue";
+  import Register from "./components/register.vue";
 
   export default {
     
@@ -27,6 +37,7 @@
 
     components: {
       "app-login" : Login,
+      "app-register" : Register,
     }
 
   
---
+++ src/components/register.vue
@@ -0,0 +1,41 @@
+<template>
+    <form @submit.prevent = "onSubmit">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name" class="form-control" v-model="userName">
+            <label for="email">Email</label>
+            <input type="text" name="email" class="form-control" v-model="userEmail">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control" v-model="userPassword">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  
+  data () {
+    return {
+        userName: "",
+        userEmail: "",
+        userPassword: ""
+    }
+  },
+
+  methods: {
+      onSubmit(){
+        axios.post('http://127.0.0.1:8000/api/auth/register', [{name: this.userName}, {email: this.userEmail},{password: this.userPassword}])
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+      }
+  }
+}
+</script>
