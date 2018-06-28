638b5ea02c7b6d40ffc8bef996970d0bc04ffddd
IlijaT <tatalovicilija@gmail.com>
2018-06-28 17:21:17 +0200

    Created login.vue component #ILTA-30

---
+++ src/components/login.vue
@@ -0,0 +1,38 @@
+<template>
+    <form @submit.prevent = "onSubmit">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" class="form-control" v-model="userEmail">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control" v-model="userPassword">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  
+  data () {
+    return {
+      userEmail: "",
+      userPassword: ""
+    }
+  },
+
+  methods: {
+      onSubmit(){
+        axios.post('http://127.0.0.1:8000/api/auth/login', [{email: this.userEmail},{password: this.userPassword}])
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+      }
+  }
+}
+</script>
