341ec47e9f3f7160576dfdc786c4ac37c768c6db
IlijaT <tatalovicilija@gmail.com>
2018-06-29 14:47:37 +0200

    Register method moved to authService.js #ILTA-31

--- src/components/register.vue
+++ src/components/register.vue
@@ -2,11 +2,11 @@
     <form @submit.prevent = "onSubmit">
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" name="name" class="form-control" v-model="userName">
+            <input type="text" name="name" class="form-control" v-model="name">
             <label for="email">Email</label>
-            <input type="email" name="email" class="form-control" v-model="userEmail">
+            <input type="email" name="email" class="form-control" v-model="email">
             <label for="password">Password</label>
-            <input type="password" name="password" class="form-control" v-model="userPassword">
+            <input type="password" name="password" class="form-control" v-model="password">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
@@ -14,27 +14,21 @@
 
 
 <script>
-import axios from 'axios';
+import { auth } from '../services/authService';
 
 export default {
   
   data () {
     return {
-        userName: "",
-        userEmail: "",
-        userPassword: ""
+        name: "",
+        email: "",
+        password: ""
     }
   },
 
   methods: {
       onSubmit(){
-        axios.post('http://127.0.0.1:8000/api/auth/register', {name: this.userName, email: this.userEmail,password: this.userPassword})
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        auth.register({name: this.name, email: this.email, password: this.password});
       }
   }
 }
